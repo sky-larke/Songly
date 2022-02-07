6d178b119d9be4205aa245e6f50f2bd28db5b9d9
Ethan Kohrt <e.a.kohrt@gmail.com>
2022-02-07 16:59:37 -0600

    committing all my changes -Ethan

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,67 +5,120 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    // Singleton stuff -----------------//
     public static GameManager instance;
     private void Awake() {
         instance = this;
     }
+    //----------------------------------//
 
-    public Text winText;
+    [Header("SET IN INSPECTOR")]
+    public GameObject spawnArea;
+    public Text winText; //text that shows up on screen when you win
+    public GameObject soundObjectPrefab;
 
+    public int maxItemsSpawned = 10;
 
-    public AudioClip theme1;
+    [Tooltip("ordered list of all goals to complete in the game")]
+    public List<SoundObjectData> allGoals;      //ordered list of all goals in the game (complete a goal by collecting all of the listed soundObject elements)
+    public SoundObjectData currentGoal;        //current soundObject to construct this round - contains data about which notes/etc to collect
+    public int currentGoalIdx;
 
-    public List<string> goal;
-
-    public List<Sprite> sprites;
-    public Dictionary<string, Sprite> images;
+    private AudioSource audioSource;
 
     private void Start() {
-        goal = new List<string>() { "A", "C", "A" };
-
-        images = new Dictionary<string, Sprite>();
-        images.Add("A", sprites[0]);
-        images.Add("B", sprites[1]);
-        images.Add("C", sprites[2]);
-        images.Add("D", sprites[3]);
-        images.Add("E", sprites[4]);
-        images.Add("G", sprites[5]);
-
-
-        GoalWindow.Instance.DisplayGoal(goal);
+        audioSource = GetComponent<AudioSource>();
+        // begin game at first goal
+        currentGoalIdx = 0;
+        currentGoal = allGoals[currentGoalIdx];
+        GoalWindow.Instance.DisplayGoal(currentGoal);
+        GenerateLevel();
     }
+
+
 
     private void Update() {
         //check if backpack equals goal?
         if (Input.GetKeyDown(KeyCode.Space)) {
             if (IsGoalMet()) {
                 Debug.Log("WIN");
-                winText.gameObject.SetActive(true);
-                SoundManager.Instance.PlaySound(theme1);
+
+                //winText.gameObject.SetActive(true);
+
+                // play sound asset
+                audioSource.clip = currentGoal.sound;
+                GetComponent<AudioSource>().Play();
+                
+                //advance to next goal
+                currentGoalIdx += 1;
+                currentGoal = allGoals[currentGoalIdx]; //TODO - this index will go out of bounds on final level
+
+                //empty backpack
+                Backpack.Instance.Clear();
+                //display new goal
+                GoalWindow.Instance.DisplayGoal(currentGoal);
+
+                //spawn new items on screen
+                Invoke("GenerateLevel", 2f);            //spawn items in new level after 2 sec delay
+
             } else {
                 Debug.Log("Not WIN");
                 Backpack.Instance.Clear();
+                Invoke("GenerateLevel", 2f); //restart current level after 2 sec dely
             }
         }
-        
-
     }
 
+ 
 
+
+
+    // Returns true if the items in backpack eqaul the required items of the goal
+    // **goal is just a SoundObjectData, which has a list of other SoundObjectData to collect. Compare by their string names.
     public bool IsGoalMet() {
-        if (goal.Count != Backpack.Instance.elements.Count) {
-            Debug.Log("Not same length: " + goal.Count + " " + Backpack.Instance.elements.Count);
+        // check that goal and backpack contain same number of elements
+        if (currentGoal.elements.Count != Backpack.Instance.elements.Count) {
+            Debug.Log("Not same length: " + currentGoal.elements.Count + " " + Backpack.Instance.elements.Count);
             return false;
         }
-        //goal = "string list"
-        //backpack is a gameObject queue?
-        for (int i = 0; i < goal.Count; i++) {
-            if (goal[i] != Backpack.Instance.elements[i].GetComponent<Note>().name) {
-                
+        // check that names of all elements are equal
+        for (int i = 0; i < currentGoal.elements.Count; i++) {
+            string targetName = currentGoal.elements[i].name;
+            string backpackName = Backpack.Instance.elements[i].name;
+            if (targetName != backpackName) {
+                Debug.Log("Target " + targetName + " != " + "Backpack" + backpackName);
                 return false;
             }
         }
         return true;
     }
 
+
+
+    // spawn SoundObjects around the screen, making sure the right items are spawned to complete the goal
+    public void GenerateLevel() {
+        ClearLevel();
+        SpawnArea area = spawnArea.GetComponent<SpawnArea>();
+        int spawnCount = 0;
+        // first spawn the necessary elements to complete the goal
+        foreach (SoundObjectData data in currentGoal.elements) {
+            Vector3 spawnPoint = area.GetRandomSpawnPoint();
+            GameObject go = Instantiate(soundObjectPrefab, spawnPoint, Quaternion.identity);
+            go.GetComponent<SoundObject>().data = data;
+            go.transform.SetParent(transform); //set as children of GameManager
+            spawnCount += 1;
+        }
+        // TODO then spawn a bunch of random things
+        for (int i = 0; i < maxItemsSpawned - spawnCount; i++) {
+            Vector3 spawnPoint = area.GetRandomSpawnPoint();
+            //Instantiate(soundObjectPrefab, spawnPoint, Quaternion.identity);
+        }
+    }
+
+    public void ClearLevel() {
+        // delete all children of GameManager (this is where we spawn the SoundObjects)
+        foreach (Transform child in transform) {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
 }
--- Assets/Scripts/GoalWindow.cs
+++ Assets/Scripts/GoalWindow.cs
@@ -9,6 +9,7 @@ public class GoalWindow : MonoBehaviour
     public Canvas elementCanvas; // set in inspector
     public GameObject imagePrefab; // must be a blank Image object
 
+    // Singleton stuff
     public static GoalWindow Instance;
     void Awake() {
         Instance = this;
@@ -27,13 +28,21 @@ public class GoalWindow : MonoBehaviour
     }
 
 
-    public void DisplayGoal(List<string> notes) {
-        foreach (string n in notes) {
-            Sprite s = GameManager.instance.images[n];
-            // instantiate image gameobject to canvas as child of elementCanvas
-            GameObject newImage = Instantiate(imagePrefab); //
-            newImage.transform.parent = elementCanvas.transform;
-            newImage.GetComponent<Image>().sprite = s;
+    public void DisplayGoal(SoundObjectData goalData) {
+        Clear();
+        foreach (SoundObjectData el in goalData.elements) {
+            // instantiate image as child of elementCanvas (so its sprite shows up in the goal window)
+            GameObject newImage = Instantiate(imagePrefab);
+            newImage.transform.SetParent(elementCanvas.transform);
+            newImage.GetComponent<Image>().sprite = el.sprite;
+        }
+    }
+
+
+    public void Clear() {
+        // delete the sprite images from the scene
+        foreach (Transform child_img in elementCanvas.transform) {
+            GameObject.Destroy(child_img.gameObject);
         }
     }
 }
